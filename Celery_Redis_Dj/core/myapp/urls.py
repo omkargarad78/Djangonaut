633b16bdac879990c3c2489b9258280b994378a8
Omkar Garad <114042405+omkargarad78@users.noreply.github.com>
2025-05-20 20:09:42 +0530

    Delete Celery_Redis_Dj directory

--- Celery_Redis_Dj/core/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-# myapp/urls.py
-
-from django.urls import path
-from .views import run_task
-
-urlpatterns = [
-    path('run-task/', run_task),
-]
--- Celery_Redis_Dj/core/myapp/views.py
+++
@@ -1,8 +0,0 @@
-# myapp/views.py
-
-from django.http import JsonResponse
-from .tasks import sleepy_task
-
-def run_task(request):
-    sleepy_task.delay(10)  # runs in background
-    return JsonResponse({"message": "Task started!"})
